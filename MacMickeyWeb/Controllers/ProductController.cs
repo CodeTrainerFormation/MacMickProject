dbe2f598bb3b4488025f4ada9002653ac9fd3364
Nab <17692533+Nab69@users.noreply.github.com>
2022-07-22 16:51:24 +0200

    ctrl + co + di

---
+++ MacMickeyWeb/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Dal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacMickeyWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MacContext context;
+
+        public ProductController(MacContext context)
+        {
+            this.context = context;
+        }
+
+        // GET : /Product/Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(context.Products.ToList());
+        }
+    }
+}
--- MacMickeyWeb/Program.cs
+++ MacMickeyWeb/Program.cs
@@ -1,7 +1,15 @@
+using Dal;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MacContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MacMickDb"));
+});
 
 var app = builder.Build();
 
